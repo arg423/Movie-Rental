ceb5dbaa0d3996dee852da66db0ac842360e4966
--12downsk <12downks@gmail.com>
2016-04-28 16:31:50 -0500

    ENUMS

---
+++ MovieRental/MovieRental/src/movierental/Genre.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierental;
+
+/**
+ *
+ * @author 12dow_000
+ */
+public enum Genre {
+    ACTION,
+    FANTASY,
+    HORROR,
+    DRAMA,
+    SCIFI,
+    ADVENTURE,
+    COMEDY
+}
---
+++ MovieRental/MovieRental/src/movierental/Rating.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierental;
+
+/**
+ *
+ * @author 12dow_000
+ */
+public enum Rating {
+    G,
+    PG,
+    PG13,
+    R
+}
