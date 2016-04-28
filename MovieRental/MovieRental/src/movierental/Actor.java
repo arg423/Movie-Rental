1d9dd6adf37597cd646fbd2929f571c9109de8c9
--12downsk <12downks@gmail.com>
2016-04-28 16:46:40 -0500

    getters

--- MovieRental/MovieRental/src/movierental/Actor.java
+++ MovieRental/MovieRental/src/movierental/Actor.java
@@ -16,4 +16,12 @@ public class Actor {
         this.name = name;
         this.gender = gender;
     }  
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public Enum getGender(){
+        return this.gender;
+    }
 }
