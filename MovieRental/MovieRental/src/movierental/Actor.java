5cc711535a5b075e38f4edfeaebf3fa3a36ab693
--12downsk <12downks@gmail.com>
2016-04-28 16:43:28 -0500

    Actor

---
+++ MovieRental/MovieRental/src/movierental/Actor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movierental;
+
+/**
+ *
+ * @author 12dow_000
+ */
+public class Actor {
+    private String name;
+    private Enum gender;
+    public Actor(String name, enum gender){
+        this.name = name;
+        this.gender = gender;
+    }
+    
+    
+}
